ee08ec6df430eff545436fab41e0e27e35fd47f6
Aka Daniel Abraham KACOU <daniel.kacou@sunu-group.com>
2023-10-06 12:19:06 +0000

    ok first launch

---
+++ src/main/java/com/testprofile/TestprofilCiApplication.java
@@ -0,0 +1,13 @@
+package com.testprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestprofilCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestprofilCiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testprofile/TestprofilCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestprofilCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
